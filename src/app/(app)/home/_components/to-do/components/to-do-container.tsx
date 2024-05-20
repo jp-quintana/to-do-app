83a39ed44b42ca637136198ed7f34ff061c79a46
jp-quintana <juanquintana1996@gmail.com>
2024-05-19 21:55:15 -0300

    add editor

--- src/app/(app)/home/_components/to-do/components/to-do-container.tsx
+++ src/app/(app)/home/_components/to-do/components/to-do-container.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Separator } from '@/components/ui/separator';
 import { ToDoHeader } from './to-do-header';
 import { ToDoList } from './to-do-list';
--- src/app/(app)/home/_components/to-do/components/to-do-list.tsx
+++ src/app/(app)/home/_components/to-do/components/to-do-list.tsx
@@ -1,3 +1,13 @@
+'use client';
+
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+
 export const ToDoList = () => {
-  return <div>ToDoList</div>;
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: '<p>Hello World! 🌎️</p>',
+  });
+
+  return <EditorContent editor={editor} />;
 };

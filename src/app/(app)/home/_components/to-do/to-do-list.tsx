b8ae3be033b8e3f023bcbb435482ddd9a557e1be
jp-quintana <juanquintana1996@gmail.com>
2024-07-06 23:52:57 -0300

    feat: update default editor content

--- src/app/(app)/home/_components/to-do/to-do-list.tsx
+++ src/app/(app)/home/_components/to-do/to-do-list.tsx
@@ -31,10 +31,7 @@ export const ToDoList = () => {
         types: ['toDoNode'],
       }),
     ],
-    content: JSON.parse(
-      window.localStorage.getItem('editor-content') ||
-        '{"type":"doc","content":[{"type":"toDoNode"}]}'
-    ),
+    content: JSON.parse(window.localStorage.getItem('editor-content') || ''),
     onUpdate: ({ editor }) => {
       if (
         editor.state.doc.textContent.trim().length === 1 &&
